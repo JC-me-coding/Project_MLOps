c46e537d637bd4da4135dd811d897dc7768f11f3
dim <dimarapis@hotmail.com>
2023-01-11 16:11:26 +0100

    fixed dataloader and wandb logger

--- src/main.py
+++ src/main.py
@@ -1,4 +1,5 @@
 from omegaconf import OmegaConf
+import os
 import torch
 import torch.nn as nn
 import sys
@@ -6,7 +7,7 @@ from tqdm import tqdm
 from torch.utils.data import DataLoader
 import wandb
 from src.model import make_model
-from src.data import make_data
+from src.data.dataloader import load_data
 from src.optimizer import make_optimizer
 from src.losses import make_loss_func
 
@@ -16,7 +17,6 @@ def train_step(net, loss_function, optimizer, data_loader, device, epoch):
     acc_sum, loss_sum, sample_num = 0, 0, 0
     
     optimizer.zero_grad()
-    
     train_bar = tqdm(data_loader, file=sys.stdout, colour='red')
     for step, data in enumerate(train_bar):
         images, labels = data
@@ -32,11 +32,12 @@ def train_step(net, loss_function, optimizer, data_loader, device, epoch):
         loss_step = loss_sum / (step + 1)
         acc_step = acc_sum / sample_num
         train_bar.desc = "[train epoch {}] loss: {:.3f}, acc: {:.3f}".format(epoch, loss_step, acc_step)
-        wandb.log({"train/loss": loss_step})
-        wandb.log({"train/acc": acc_step})
-
+        #wandb.log({"train/loss": loss_step})
+        #wandb.log({"train/acc": acc_step})
     
-    return train_loss / (step + 1), acc_sum / sample_num
+    wandb.log({"epoch/train/loss": loss_step})
+    wandb.log({"epoch/train/acc": acc_step})
+    return loss_sum / (step + 1), acc_sum / sample_num
         
         
 @torch.no_grad()
@@ -59,8 +60,10 @@ def val_step(net, loss_function, data_loader, device, epoch):
 
         # Logging
         val_bar.desc = "[valid epoch {}] loss: {:.3f}, acc: {:.3f}".format(epoch, loss_step, acc_step)
-        wandb.log({"val/loss": loss_step})
-        wandb.log({"val/acc": acc_step})
+        #wandb.log({"val/loss": loss_step})
+        #wandb.log({"val/acc": acc_step})
+    wandb.log({"epoch/test/loss": loss_step})
+    wandb.log({"epoch/test/acc": acc_step})
         
     return loss_sum / (step + 1), acc_sum / sample_num
 
@@ -68,48 +71,48 @@ def val_step(net, loss_function, data_loader, device, epoch):
 if __name__ == '__main__':
 
     ############# CONFIG #############
-    # ToDo: Extract config
-    config = OmegaConf.from_dotlist([
-        "batch_size=32",
-        "epochs=5",
-        "weight_decay=1e-5",
-        "lr=0.001",
-        "num_epochs=3",
-        "loss_fun=cross_entropy",  # l1_loss, cross_entropy, ...
-        "val_interval=5"
-        "optimizer=adam",  # adam, sgd, ... 
-    ])
+    config = OmegaConf.load('src/model_config.yaml')
+ 
+    #ToDo: Put params into code. Quick fix for now, grouping all params
+    backbone = config.model.backbone
+    batch_size = config.data.batch_size
+    optimizer = config.training.optimizer
+    epochs = config.training.epochs
+    weight_decay = config.hyperparameters.weight_decay
+    lr = config.hyperparameters.learning_rate
+    num_epochs = config.training.epochs
+    loss_fun = config.training.loss_fun
+    val_interval = config.training.val_interval
 
-    ############# DATA #############
-    root = "data/raw/landscapes"
     
-    train_dataset = make_data(root, split="train")
-    train_loader = DataLoader(train_dataset, batch_size=config.batch_size, shuffle=True, num_workers=0)
-
-    valid_dataset = make_data(root, split="val")
-    valid_loader = DataLoader(valid_dataset, batch_size=config.batch_size, shuffle=False, num_workers=0)
-
+    ############# DATA #############
+    root = "data/processed/landscapes"
+    assert os.path.exists(root), "Download and extract the data first, using the script in src/data/data.py"
+    
+    train_loader = load_data(root, split="train", batch_size = batch_size)
+    valid_loader = load_data(root, split="val", batch_size = 1)
+    
     ############# MODEL #############
     device = 'cuda' if torch.cuda.is_available() else 'cpu'
-    net = make_model('resnet34', pretrained=True)
+    net = make_model(backbone, pretrained=True).to(device)
 
     ############# LOSS FUNCTION #############
-    loss_function = make_loss_func(config.loss_fun)
-
+    loss_function = make_loss_func(loss_fun)
     ############# OPTIMIZER #############
-    optimizer = make_optimizer(config.optimizer, net, config)
-
-    wandb.init(config=config)
-    # Magic
-    wandb.watch(net, log_freq=100)
+    optimizer = make_optimizer(optimizer, net, config)
+    #print(optimizer)
+    wandb.init(project=config.wandb.project, entity=config.wandb.entity)
+    wandb.config = OmegaConf.to_container(config, resolve=True, throw_on_missing=True)
+    #Magic
+    #wandb.watch(net, log_freq=100)
     
     ############# TRAINING #############
     best_val_acc = 0
-    for epoch in range(config.num_epochs):
+    for epoch in range(num_epochs):
         train_loss, train_accuracy = train_step(net, loss_function, optimizer, train_loader, device, epoch)
 
-        if epoch % config.val_interval == 0:
+        if epoch % val_interval == 0:
             val_loss, val_accuracy = val_step(net, loss_function, valid_loader, device, epoch)
             if val_accuracy > best_val_acc:
                 best_val_acc = val_accuracy
-                torch.save(net.state_dict(), f'./model_{epoch}.pth')
+                torch.save(net.state_dict(), f'./models/model_{epoch}.pth')
--- src/model.py
+++ src/model.py
@@ -4,3 +4,4 @@ NUM_CLASSES = 5
 
 def make_model(model_name, pretrained=True):
     net = timm.create_model(model_name, num_classes=NUM_CLASSES, pretrained=pretrained)
+    return net

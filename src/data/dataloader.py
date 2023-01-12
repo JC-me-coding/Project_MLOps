d74ac457dc606ff429099b2755233c41fdebda93
Julia Cathrine Youngman <juca@nnit.com>
2023-01-12 15:15:46 +0100

    add pep8 checks

--- src/data/dataloader.py
+++ src/data/dataloader.py
@@ -1,27 +1,38 @@
 from torchvision import datasets, transforms
 from torch.utils.data import DataLoader
+
 # ToDo: Use more exotic augmentations from timm5
 def get_train_transforms():
     transform_list = []
     transform_list.append(transforms.RandomResizedCrop(224))
     transform_list.append(transforms.ToTensor())
     transform_list.append(transforms.RandomHorizontalFlip())
-    transform_list.append(transforms.Normalize([0.485, 0.456, 0.406], [0.229, 0.224, 0.225]))
+    transform_list.append(
+        transforms.Normalize([0.485, 0.456, 0.406], [0.229, 0.224, 0.225])
+    )
     return transforms.Compose(transform_list)
+
 
 def get_val_transforms():
     transform_list = []
-    #transform_list.append(transforms.Resize(224, 244))# Does not make sense with CenterCrop, right?
+    # transform_list.append(transforms.Resize(224, 244))# Does not make sense with CenterCrop, right?
     transform_list.append(transforms.CenterCrop(224))
     transform_list.append(transforms.ToTensor())
-    transform_list.append(transforms.Normalize([0.485, 0.456, 0.406], [0.229, 0.224, 0.225]))
+    transform_list.append(
+        transforms.Normalize([0.485, 0.456, 0.406], [0.229, 0.224, 0.225])
+    )
     return transforms.Compose(transform_list)
+
 
 def load_data(root, split, batch_size):
     if split == "train":
-        data = datasets.ImageFolder(f"{root}/Training Data", get_train_transforms()) 
+        data = datasets.ImageFolder(f"{root}/Training Data", get_train_transforms())
     elif split == "val":
-        data = datasets.ImageFolder(f"{root}/Validation Data", get_val_transforms()) 
-        
-    return DataLoader(data, batch_size=batch_size, shuffle=True, drop_last = True if split=="train" else False)
-    
+        data = datasets.ImageFolder(f"{root}/Validation Data", get_val_transforms())
+
+    return DataLoader(
+        data,
+        batch_size=batch_size,
+        shuffle=True,
+        drop_last=True if split == "train" else False,
+    )
--- src/data/make_dataset.py
+++ src/data/make_dataset.py
@@ -9,71 +9,74 @@ import torchvision.transforms as transforms
 import torchvision.datasets as datasets
 from torch.utils.data import DataLoader
 
+
 @click.command()
-@click.argument('input_filepath', type=click.Path(exists=True))
-@click.argument('output_filepath', type=click.Path())
+@click.argument("input_filepath", type=click.Path(exists=True))
+@click.argument("output_filepath", type=click.Path())
 def main(input_filepath, output_filepath):
-    """ Runs data processing scripts to turn raw data from (../raw) into
-        cleaned data ready to be analyzed (saved in ../processed).
+    """Runs data processing scripts to turn raw data from (../raw) into
+    cleaned data ready to be analyzed (saved in ../processed).
     """
     logger = logging.getLogger(__name__)
-    logger.info('making final data set from raw data')
+    logger.info("making final data set from raw data")
 
     torch.manual_seed(0)
     batch_size = 64
 
     # training transforms
-    train_transform = transforms.Compose([
-        #transforms.Resize((x,y)), #TODO??
-        #transforms.RandomHorizontalFlip(p=0.1),
-        #transforms.RandomVerticalFlip(p=0.1),
-        #transforms.GaussianBlur(kernel_size=(5, 9), sigma=(0.1, 5)),
-        #transforms.RandomRotation(degrees=(10, 20)),
-        transforms.ToTensor(),
-        transforms.Normalize(
-            mean=[0.5, 0.5, 0.5],
-            std=[0.5, 0.5, 0.5]
-            )
-    ])
+    train_transform = transforms.Compose(
+        [
+            # transforms.Resize((x,y)), #TODO??
+            # transforms.RandomHorizontalFlip(p=0.1),
+            # transforms.RandomVerticalFlip(p=0.1),
+            # transforms.GaussianBlur(kernel_size=(5, 9), sigma=(0.1, 5)),
+            # transforms.RandomRotation(degrees=(10, 20)),
+            transforms.ToTensor(),
+            transforms.Normalize(mean=[0.5, 0.5, 0.5], std=[0.5, 0.5, 0.5]),
+        ]
+    )
 
     # validation transforms
-    valid_transform = transforms.Compose([
-        #transforms.Resize((x,y)), #TODO??
-        transforms.ToTensor(),
-        transforms.Normalize(
-            mean=[0.5, 0.5, 0.5],
-            std=[0.5, 0.5, 0.5]
-        )
-    ])
+    valid_transform = transforms.Compose(
+        [
+            # transforms.Resize((x,y)), #TODO??
+            transforms.ToTensor(),
+            transforms.Normalize(mean=[0.5, 0.5, 0.5], std=[0.5, 0.5, 0.5]),
+        ]
+    )
 
     # training dataset
     train_dataset = datasets.ImageFolder(
-        root=input_filepath + '/Training Data',
-        transform=train_transform
+        root=input_filepath + "/Training Data", transform=train_transform
     )
     # validation dataset
     valid_dataset = datasets.ImageFolder(
-        root=input_filepath + '/Validation Data',
-        transform=valid_transform
+        root=input_filepath + "/Validation Data", transform=valid_transform
     )
     # training data loaders
     train_loader = DataLoader(
-        train_dataset, batch_size=batch_size, shuffle=True,
-        num_workers=4, pin_memory=True
+        train_dataset,
+        batch_size=batch_size,
+        shuffle=True,
+        num_workers=4,
+        pin_memory=True,
     )
     # validation data loaders
     valid_loader = DataLoader(
-        valid_dataset, batch_size=batch_size, shuffle=False,
-        num_workers=4, pin_memory=True
+        valid_dataset,
+        batch_size=batch_size,
+        shuffle=False,
+        num_workers=4,
+        pin_memory=True,
     )
 
     # save dataloaders
-    torch.save(train_loader, output_filepath + '/train_dl.pt')
-    torch.save(valid_loader, output_filepath + '/validation_dl.pt')
+    torch.save(train_loader, output_filepath + "/train_dl.pt")
+    torch.save(valid_loader, output_filepath + "/validation_dl.pt")
 
 
-if __name__ == '__main__':
-    log_fmt = '%(asctime)s - %(name)s - %(levelname)s - %(message)s'
+if __name__ == "__main__":
+    log_fmt = "%(asctime)s - %(name)s - %(levelname)s - %(message)s"
     logging.basicConfig(level=logging.INFO, format=log_fmt)
 
     # not used in this stub but often useful for finding various files
